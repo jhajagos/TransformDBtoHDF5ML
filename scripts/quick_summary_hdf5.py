e0ab8d6e4eb822106bf96847bbfd897184effd22
Janos G. Hajagos <risk.limits@gmail.com>
2017-05-04 21:40:01 -0400

    added unique count flag

--- scripts/quick_summary_hdf5.py
+++ scripts/quick_summary_hdf5.py
@@ -10,17 +10,16 @@ except ImportError:
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0], os.path.pardir)))
     from prediction_matrix_generate.utility_functions import get_all_paths
 
-
 import csv
 import h5py
 import numpy as np
 import argparse
 
-
 #TODO: Add path list
-def main(hdf5_filename, csv_file_name=None, threshold_value_to_include=0.01):
-    fp5 = h5py.File(hdf5_filename)
+def main(hdf5_filename, csv_file_name=None, threshold_value_to_include=0.01,
+         unique_count_field_path=None, unique_column_index=0):
 
+    fp5 = h5py.File(hdf5_filename)
     paths = get_all_paths(fp5["/"])
 
     core_array_paths = [p for p in paths if p.split("/")[-1] == "core_array"]
@@ -28,12 +27,17 @@ def main(hdf5_filename, csv_file_name=None, threshold_value_to_include=0.01):
     stripped_split_paths = [p.split("/")[:-1] for p in core_array_paths]
     stripped_paths = ["/".join(sp) for sp in stripped_split_paths]
 
+    if unique_count_field_path:
+        unique_column_array = fp5[unique_count_field_path][:, unique_column_index]
+        unique_array = np.unique(unique_column_array)
+        unique_column_count = unique_array.shape[0]
+
     if csv_file_name is None:
         csv_file_name = hdf5_filename + ".summary.csv"
 
         with open(csv_file_name, "wb") as fw:
             csv_writer = csv.writer(fw)
-            header = ["path", "c1", "c2", "c3", "non-zero", "to_include", "fraction non-zero"]
+            header = ["path", "c1", "c2", "c3", "c4", "non-zero", "to_include", "fraction non-zero", "unique", "fraction_unique"]
             csv_writer.writerow(header)
             for stripped_path in stripped_paths:
                 print(stripped_path)
@@ -45,6 +49,7 @@ def main(hdf5_filename, csv_file_name=None, threshold_value_to_include=0.01):
                     slice_of_interest = fp5[core_array_path][:, j]
                     number_of_rows = slice_of_interest.shape[0]
                     non_zero_values = np.where(slice_of_interest > 0)
+
                     n_non_zero_values = len(non_zero_values[0])
                     column_name_to_write = column_annotations[:,j]
                     column_names = column_name_to_write.transpose().tolist()
@@ -57,6 +62,10 @@ def main(hdf5_filename, csv_file_name=None, threshold_value_to_include=0.01):
 
                     row_to_write = [stripped_path] + column_names + [n_non_zero_values, to_include, fraction_non_zero]
 
+                    if unique_count_field_path is not None:
+                        number_of_unique_values_in_columns = np.unique(unique_column_array[non_zero_values]).shape[0]
+                        row_to_write += [number_of_unique_values_in_columns, (number_of_unique_values_in_columns * 1.0) / unique_column_count]
+
                     csv_writer.writerow(row_to_write)
 
 
@@ -65,8 +74,10 @@ if __name__ == "__main__":
 
     argparse_obj.add_argument("-f", "--hdf5-filename", dest="hdf5_filename", help="HDF5 file to summarize")
     argparse_obj.add_argument("-m", "--mark-frequency-by-fraction", dest="threshold_value_to_include", default=0.01, type=float,
-                              help="Mark records that occur at a fraction of at least between 0 and 1"
-                             )
+                              help="Mark records that occur at a fraction of at least between 0 and 1")
+
+    argparse_obj.add_argument("-u", "--unique-count-field-path", dest="unique_count_field_path", default=None)
 
     arg_obj = argparse_obj.parse_args()
-    main(hdf5_filename=arg_obj.hdf5_filename, threshold_value_to_include=arg_obj.threshold_value_to_include)
+    main(hdf5_filename=arg_obj.hdf5_filename, threshold_value_to_include=arg_obj.threshold_value_to_include,
+         unique_count_field_path=arg_obj.unique_count_field_path)
